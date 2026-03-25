40877ef045fdb44b4e2cf5179087729a593fed58
shivam-jainn <shivam.clgstash@gmail.com>
2026-03-25 16:22:04 +0530

    chore : removed imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
 import Landing from './components/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <Landing />
